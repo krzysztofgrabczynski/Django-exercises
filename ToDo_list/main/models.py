d2fc23e6b2593064c3d805620f68e8205256d3a0
Krzysztof Grabczynski <grabczynski.krzysztof@gmail.com>
2023-02-25 16:03:49 +0100

    Goal model

--- ToDo_list/main/models.py
+++ ToDo_list/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Goal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+    title = models.CharField(max_length=256, blank=False)
+    details = models.TextField(blank=False)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
