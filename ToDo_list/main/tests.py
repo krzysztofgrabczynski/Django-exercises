0929de0b2f4cabb5355848ff568cce9de838388c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-02-26 22:11:35 +0100

    First tests for models

--- ToDo_list/main/tests.py
+++ ToDo_list/main/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import Goal, GoalsList, User
 
-# Create your tests here.
+class TestModels(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        user = User.objects.create_user(username='test_user', password='testPassword123!')
+        GoalsList.objects.create(user=user)
+        Goal.objects.create(
+            user=user,
+            title='Test Goal',
+            details='Test Details',
+        )
+
+    # test for Goal model
+    def test_model_Goal__str__(self):
+        goal = Goal.objects.first()
+        expected = goal.title
+
+        self.assertEqual(expected, str(goal))
+
+    # test for GoalsList model
+    def test_model_GoalsList__str__(self):
+        goals_list = GoalsList.objects.first()
+        expected = f"{goals_list.user}'s goals list"
+
+        self.assertEqual(expected, str(goals_list))
+
+    def test_model_GoalsList_goals_list_property(self):
+        goals_list = GoalsList.objects.first().goals_list
+        goal = Goal.objects.first()
+        expected = [goal]
+
+        self.assertEqual(expected, goals_list)
+
+    def test_model_GoalsList_goals_list_len(self):
+        goals_list = GoalsList.objects.first()
+
+        self.assertEqual(goals_list.goals_list_len(), 1)    
