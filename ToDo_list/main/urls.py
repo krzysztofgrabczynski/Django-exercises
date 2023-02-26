7b3cb4a1b5e1f7eb5c631445410f7c056f71cb0b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-02-26 23:14:44 +0100

    Memento funtionality demo version

--- ToDo_list/main/urls.py
+++ ToDo_list/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('undo/', views.undo, name='undo'),
+    path('redo/', views.redo, name='redo'),
+    path('save/', views.save, name='save'),
 ]
--- ToDo_list/main/views.py
+++ ToDo_list/main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from .models import Goal, GoalsList
 
 def home(request):
-    return HttpResponse('test')
+    return HttpResponse('home')
+
+def undo(request):
+    return HttpResponse('undo')
+
+def redo(request):
+    return HttpResponse('redo')
+
+def save(request):
+    return HttpResponse('save')
+
+
+
