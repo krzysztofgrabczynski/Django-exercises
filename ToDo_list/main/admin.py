6b5d5d5813ed4346c58d715205225786736c2978
Krzysztof Grabczynski <grabczynski.krzysztof@gmail.com>
2023-02-26 12:45:02 +0100

    GoalsList model

--- ToDo_list/main/admin.py
+++ ToDo_list/main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Goal
+from .models import Goal, GoalsList
 
 admin.site.register(Goal)
+admin.site.register(GoalsList)
--- ToDo_list/main/models.py
+++ ToDo_list/main/models.py
@@ -1,15 +1,35 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
     title = models.CharField(max_length=256, blank=False)
     details = models.TextField(blank=False)
     date = models.DateField(auto_now_add=True)
-
+    
     def __str__(self) -> str:
         return self.title
     
     def __repr__(self) -> str:
         return self.__str__()
 
+
+class GoalsList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals_list')
+    id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+
+    def __str__(self) -> str:
+        return f"{self.user}'s goals list"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    @property
+    def goals_list(self):
+        goals = Goal.objects.filter(user=self.user)
+        return list(goals)
+
+    def goals_list_len(self):
+        return len(self.goals_list)
