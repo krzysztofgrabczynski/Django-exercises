0ac31c825507c5dab968c5874394c82776829425
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-03-12 21:57:35 +0100

    Adding ListView

--- Django_filters/main/views.py
+++ Django_filters/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Film
 from .filters import FilmFilter
+from django.views.generic import ListView
 
 
 def index(request, *args, **kwargs):
@@ -11,3 +12,19 @@ def index(request, *args, **kwargs):
         'films': film_filter.qs
     }
     return render(request, 'index.html', context)
+
+
+class FilmListView(ListView):
+    template_name = 'index.html'
+    queryset = Film.objects.all()
+    context_object_name = 'films'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = FilmFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = self.filterset.form
+        return context
