05356d19c50a73112fa2aaeef49ba7880cfe147e
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-03-12 20:02:09 +0100

    Film model and new migration

--- Django_filters/main/models.py
+++ Django_filters/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Film(models.Model):
+    class CategoryChoices(models.TextChoices):
+        SCI_FI = 'SF'
+        DRAMA = 'D'
+        ADVENTURE = 'AV'
+        COMEDY = 'C'
+        ACTION = 'A'
+        OTHER = 'O'
+        
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
+    titel = models.CharField(max_length=128, blank=False)
+    category = models.CharField(max_length=2, choices=CategoryChoices.choices, default=CategoryChoices.OTHER)
+    description = models.TextField(blank=False)
+    year = models.PositiveSmallIntegerField(blank=False, default='2000')
+    premiere = models.DateField(blank=False)
+    imdb_rating = models.DecimalField(blank=False, max_digits=4, decimal_places=2)
+    director = models.CharField(max_length=256, blank=False)
+
