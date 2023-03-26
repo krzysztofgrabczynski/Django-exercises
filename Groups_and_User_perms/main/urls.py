0a4a6036ded6bd4349d562720fd89042351c4bef
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-03-26 20:45:25 +0200

    Adding url, empty views and refactor templates for edit and delete action

--- Groups_and_User_perms/main/urls.py
+++ Groups_and_User_perms/main/urls.py
@@ -5,5 +5,6 @@ from main import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('create_obj/', views.create_obj, name='create_obj'),
-  
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
--- Groups_and_User_perms/main/views.py
+++ Groups_and_User_perms/main/views.py
@@ -16,12 +16,8 @@ def create_obj(request):
 
     if request.method == 'POST':
         if form.is_valid():
-            print('po valid')
             obj_type = form.cleaned_data['type']
-            print(obj_type)
-            print(BaseModel.TypeChoices.FILM.value)
             if int(obj_type) == BaseModel.TypeChoices.FILM.value:
-                print('w film')
                 Film.objects.get_or_create(
                     title = form.cleaned_data['title'],
                     description = form.cleaned_data['description'],
@@ -30,7 +26,6 @@ def create_obj(request):
                     is_available = form.cleaned_data['is_available'],
                 )
             elif int(obj_type) == BaseModel.TypeChoices.BOOK.value:
-                print('w book')
                 Book.objects.create(
                     title = form.cleaned_data['title'],
                     description = form.cleaned_data['description'],
@@ -39,7 +34,6 @@ def create_obj(request):
                     is_available = form.cleaned_data['is_available'],
                 )
             elif int(obj_type) == BaseModel.TypeChoices.ARTICLE.value:
-                print('w article')
                 Article.objects.create(
                     title = form.cleaned_data['title'],
                     description = form.cleaned_data['description'],
@@ -47,12 +41,13 @@ def create_obj(request):
                     price = form.cleaned_data['price'],
                     is_available = form.cleaned_data['is_available'],
                 )
-    # form.is_valid():
-    #     print(form.type)
-    #     form.type = 3
-    #     print(form.type)
-    #     form.save()
 
             return redirect(index)
     
     return render(request, 'create_obj.html', {'form': form})
+
+def edit(request, id):
+    pass
+
+def delete(request, id):
+    pass
