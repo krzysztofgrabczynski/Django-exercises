4f26155a43fbeabfd5c0438da7926fa3a0958bba
Krzysztof Grabczynski <grabczynski.krzysztof@gmail.com>
2023-03-26 13:44:51 +0200

    New models

--- Groups_and_User_perms/main/models.py
+++ Groups_and_User_perms/main/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    class TypeChoices(models.IntegerChoices):
+        FILM = 1
+        BOOK = 2
+        ARTICLE = 3
+
+    title = models.CharField(max_length=254)
+    description = models.TextField()
+    type = models.CharField(max_length=7, choices=TypeChoices.choices)
+    price = models.FloatField()
+    is_available = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    @classmethod
+    def get_all_objects(cls):
+        objects = []
+        for subclass in cls.__subclasses__():
+            subclass_objects = subclass.objects.all()
+            if subclass_objects:
+                objects.append(subclass_objects)
+                
+        return objects
+
+
+class Film(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.type = self.TypeChoices.FILM
+
+class Book(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.type = self.TypeChoices.BOOK
+
+class Article(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.type = self.TypeChoices.ARTICLE
