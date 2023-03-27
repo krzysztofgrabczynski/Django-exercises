234244a6c3339685940d3fc38500d5fb9e5bb369
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-03-27 14:56:20 +0200

    Edit functionality

--- Groups_and_User_perms/main/forms.py
+++ Groups_and_User_perms/main/forms.py
@@ -2,11 +2,14 @@ from django import forms
 from .models import BaseModel, Film
 
 class AddNewForm(forms.Form):
-    # class Meta:
-    #     model = Film
-    #     fields = ['title', 'description', 'type', 'price', 'is_available']
     title = forms.CharField(max_length=254)
     description = forms.CharField(widget=forms.Textarea)
     type = forms.ChoiceField(choices=BaseModel.TypeChoices.choices)
     price= forms.FloatField()
     is_available = forms.BooleanField(required=False)
+
+    def set_instance(self, instance):
+        self.instance = instance
+        for key in self.fields:
+            if hasattr(instance, key):
+                self.fields[key].initial = getattr(instance, key)
--- Groups_and_User_perms/main/views.py
+++ Groups_and_User_perms/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import BaseModel, Film, Book, Article
 from .forms import AddNewForm
 
@@ -50,14 +50,36 @@ def create_obj(request):
     return render(request, 'create_obj.html', {'form': form})
 
 def edit(request, id):
-    pass
+    result = None
+    with BaseModel.SubclassesContextManager() as manager:
+        for obj in manager:
+            if not result:
+                result = obj.objects.filter(id=id).first()
+
+    get_obj = get_object_or_404(result.__class__, pk=id)
+    form = AddNewForm(request.POST or None)
+    form.set_instance(get_obj)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            Book.objects.update(
+                title = form.cleaned_data['title'],
+                description = form.cleaned_data['description'],
+                type = 1,
+                price = form.cleaned_data['price'],
+                is_available = form.cleaned_data['is_available'],
+            )
+
+        return redirect(index)
+    
+    return render(request, 'edit_obj.html', {'form': form})
 
 def delete(request, id):
     result = None
     with BaseModel.SubclassesContextManager() as manager:
         for obj in manager:
             if not result:
-                result = obj.objects.filter(id=id)
+                result = obj.objects.filter(id=id).first()
 
     result.delete()
     return redirect(index)
