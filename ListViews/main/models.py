d03f978f803906bf93327fa9cbe7cc22f0478938
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-04-03 22:53:49 +0200

    Product model

--- ListViews/main/models.py
+++ ListViews/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class TypeChoices(models.TextChoices):
+        FOOD = 'F'
+        ELECTRONIC = 'E'
+        CLOTHES = 'C'
+        BOOK = 'B'
+        OTHER = 'O'
+
+    name = models.CharField(max_length=254)
+    price = models.FloatField()
+    description = models.TextField()
+    type = models.CharField(max_length=10, choices=TypeChoices.choices)
+    
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def __repr__(self) -> str:
+        return self.__str__()
