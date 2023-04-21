7c4eda03bcb1bfbc14f967b9c460acf05e8dd8ba
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-04-21 18:52:45 +0200

    Book model

--- ClassBasedViews/main/models.py
+++ ClassBasedViews/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class BookModel(models.Model):
+    title = models.CharField(max_length=254, unique=True)
+    slug = models.SlugField(max_length=254, blank=False)
+    author = models.CharField(max_length=254)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.__str__()
+
