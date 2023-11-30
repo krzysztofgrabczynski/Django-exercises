49a558ad48596925b3e8fb0b3033f8c4762f6aee
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-11-30 18:18:27 +0100

    Creating new file mixins.py and decorator that checks of user in request is logged in

---
+++ TripIdeas_project/app/mixins.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect
+from functools import wraps
+
+
+def if_logged_user(func):
+    """
+    Decorator that checks if a user in request is logged. If user is logged it will redirect him to "home" page.
+    """
+
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("home")
+        
+        return func(request, *args, **kwargs)
+    return wrapper
