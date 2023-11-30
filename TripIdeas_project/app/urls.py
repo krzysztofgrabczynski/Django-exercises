42cc7224fbe27a7d9551c77a5f30709ef0cfcf34
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-11-30 18:18:50 +0100

    New urls for login and logout

--- TripIdeas_project/app/urls.py
+++ TripIdeas_project/app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from app import views as app_views
+from app.mixins import if_logged_user
 
 
 urlpatterns = [
     path("home/", app_views.home, name="home"),
     path("sign-up/", app_views.CreateUserFormView.as_view(), name="sign_up"),
+    path("login/", if_logged_user(LoginView.as_view()), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 
 ]
