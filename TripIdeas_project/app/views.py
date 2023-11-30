b30e208032b007355a54f08676a6a1e226a240b4
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-11-30 17:36:53 +0100

    Creating View CreateUserFormView for creating new user

--- TripIdeas_project/app/views.py
+++ TripIdeas_project/app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+from django.contrib.auth import login
 
-# Create your views here.
+from app.forms import UserRegistrationForm
+from app.models import UserProfile
+
+
+def home(request):
+    return HttpResponse("home page")
+
+
+class CreateUserFormView(generic.FormView):
+    template_name = "registration/sign_up.html"
+    form_class = UserRegistrationForm
+    success_url = "home/"
+
+
+    def form_valid(self, form):
+        user = form.save()
+        phone_number = form.cleaned_data["phone_number"]
+        
+        UserProfile.objects.create(user=user, phone_number=phone_number)
+    
+        return super().form_valid(form)
