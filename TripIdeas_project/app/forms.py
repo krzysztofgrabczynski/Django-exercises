d00b5cf553417ba2bb392b6e1a3798ca4f353327
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-06 23:19:13 +0100

    New form for forget_password functionality with validation for email field if specific email exists in db:

--- TripIdeas_project/app/forms.py
+++ TripIdeas_project/app/forms.py
@@ -8,7 +8,7 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "password1", "password2"]
+        fields = ["username", "password1", "password2", "email"]
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data["phone_number"]
@@ -16,3 +16,14 @@ class UserRegistrationForm(UserCreationForm):
             raise forms.ValidationError("Phone number must be a number with 9 digits")
 
         return phone_number
+
+
+class ForgetPasswordForm(forms.Form):
+    email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if not User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email address does not exist")
+        
+        return email
