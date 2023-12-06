b889803d9593f298d94f25ac8ab01a42eea0af78
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-06 23:19:29 +0100

    Create ForgetPasswordView

--- TripIdeas_project/app/views.py
+++ TripIdeas_project/app/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 from django.http import HttpResponse
 from django.contrib.auth import login
 
-from app.forms import UserRegistrationForm
+from app.forms import UserRegistrationForm, ForgetPasswordForm
 from app.models import UserProfile
 
 
@@ -23,3 +23,13 @@ class CreateUserFormView(generic.FormView):
         login(self.request, user)
 
         return super().form_valid(form)
+
+
+class ForgetPasswordView(generic.FormView):
+    template_name = "registration/forget_password.html"
+    form_class = ForgetPasswordForm
+    success_url = "/app/login/"
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
