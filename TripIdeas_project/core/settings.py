e7f558f71d296a07cad381801d7c56c5a829e6fa
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-11 23:33:05 +0100

    Added email settings

--- TripIdeas_project/core/settings.py
+++ TripIdeas_project/core/settings.py
@@ -116,4 +116,8 @@ CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 # Email settings
-EMAIL_HOST_USER = ...
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
