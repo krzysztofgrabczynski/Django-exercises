f529caf17c6105f910051c960396b554b27865da
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-11 23:34:41 +0100

    Customization of the 'PasswordResetForm' by changing 'send_mail' method to work with celery and redis

--- TripIdeas_project/app/forms.py
+++ TripIdeas_project/app/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django.contrib.auth.models import User
+from django.template import loader
+
+from app.tasks import send_reset_password_email_task
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -18,12 +21,20 @@ class UserRegistrationForm(UserCreationForm):
         return phone_number
 
 
-class ForgetPasswordForm(forms.Form):
-    email = forms.EmailField()
+class CustomPasswordResetForm(PasswordResetForm):
+    """
+    Custumized `PasswordResetForm` class. Changed `send_mail` method to work with celery and redis (asynchronous email sending).
+    """
+    
+    def send_mail(
+        self,
+        subject_template_name,
+        email_template_name,
+        context,
+        from_email,
+        to_email,
+        html_email_template_name=None,
+    ):
+        message = loader.render_to_string(email_template_name, context)
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
-        if not User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email address does not exist")
-        
-        return email
+        send_reset_password_email_task.delay(message, from_email, to_email)
