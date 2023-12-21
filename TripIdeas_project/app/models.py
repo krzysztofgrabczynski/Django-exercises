ccba2ec9374eddd5f1e0b625843c62338034edd0
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-21 17:12:52 +0100

    Update of the

--- TripIdeas_project/app/models.py
+++ TripIdeas_project/app/models.py
@@ -11,7 +11,13 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(null=False, blank=False, max_length=9)
 
+
 class FloatFieldWithTracking(models.FloatField):
+    """
+    Descriptor class.
+    Customized `models.FloatField` class with tracking functionality to store all modified values in given in `track_attribute_name` attribute.
+    """
+    
     def __init__(self, track_attribute_name: str, *args: Any, **kwargs: Any) -> None:
         self._name = None
         self.track_attribute_name = track_attribute_name
@@ -37,15 +43,23 @@ class FloatFieldWithTracking(models.FloatField):
     def add_datetime_to_value(self, value):
         return (value, datetime.now())
 
-
-class TripModel(models.Model):
-    owner = ...
-    name = ...
-    destination = ...
-    description = ...
-    price = ...
-    is_abroad = ...
-    contact_number = ...
+    def values_between_dates(self, start: datetime, end: datetime) -> list:
+        values = self.__dict__[self.track_attribute_name]
+        return [x for x in values if start < x < end]
     
 
+class TripModel(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(max_length=128, null=False, blank=False)
+    destination = models.CharField(max_length=128, null=False, blank=False)
+    description = models.TextField(default="")
+    price = FloatFieldWithTracking(track_attribute_name="trip_price_history", null=False, blank=False)
+    is_abroad = models.BooleanField(null=False, blank=False)
+    contact_number = models.CharField(max_length=9, null=False, blank=False)
+    
+    def min_price_between_dates(self, start:datetime, end:datetime):
+        return min(self.price.values_between_dates(start, end))
+            
+    def get_last_price(self):
+        return self.price.track_attribute_name[-2]
     
