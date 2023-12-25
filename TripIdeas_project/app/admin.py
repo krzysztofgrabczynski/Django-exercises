3aec29152f2a5b060351335bbff6f497e9b346ee
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-25 19:31:01 +0100

    Site register of the new models

--- TripIdeas_project/app/admin.py
+++ TripIdeas_project/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from app.models import UserProfile
+from app.models import UserProfile, PriceModelWithTracker, TripModel
 
 
 admin.site.register(UserProfile)
+admin.site.register(PriceModelWithTracker)
+admin.site.register(TripModel)
