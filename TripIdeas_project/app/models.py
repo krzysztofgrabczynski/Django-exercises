6426461ff77f235181aafb43ff67568c133bf161
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-12-25 19:33:46 +0100

    Update of the price tracker field: deleted descriptor and used django-field-hisory for tracking price field

--- TripIdeas_project/app/models.py
+++ TripIdeas_project/app/models.py
@@ -1,7 +1,7 @@
-from typing import Any
 from django.db import models
 from django.contrib.auth import get_user_model
 from datetime import datetime
+from field_history.tracker import FieldHistoryTracker
 
 
 User = get_user_model()
@@ -12,54 +12,47 @@ class UserProfile(models.Model):
     phone_number = models.CharField(null=False, blank=False, max_length=9)
 
 
-class FloatFieldWithTracking(models.FloatField):
-    """
-    Descriptor class.
-    Customized `models.FloatField` class with tracking functionality to store all modified values in given in `track_attribute_name` attribute.
-    """
-    
-    def __init__(self, track_attribute_name: str, *args: Any, **kwargs: Any) -> None:
-        self._name = None
-        self.track_attribute_name = track_attribute_name
-        super().__init__(*args, **kwargs)
+class PriceModelWithTracker(models.Model):
+    price = models.FloatField(null=True, blank=False)
+    price_history = FieldHistoryTracker(["price"])
 
-    def __set_name__(self, owner, name):
-        self._name = name
+    def __str__(self):
+        return str(self.price)
 
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        return instance.__dict__[self._name]
+    def __repr__(self):
+        return self.__str__()
 
-    def __set__(self, instance, value):
-        expanded_value = self.add_datetime_to_value(value)
-        if not hasattr(instance, self.track_attribute_name):
-            instance.__dict__[self.track_attribute_name] = [expanded_value]
+    def min_price_between_dates(self, start: datetime.date, end: datetime.date):
+        try:
+            return min(
+                (
+                    x.field_value
+                    for x in self.price_history.all()
+                    if start <= x.date_created.date() <= end
+                )
+            )
+        except ValueError as e:
+            raise ValueError(
+                "Probably there is no prices between provided dates. Raises: %s" % e
+            )
+        except TypeError:
+            raise TypeError(
+                "Error in `min_price_between_dates` method: `start` or `end` value are not `datetime.date` type."
+            )
+
+    def get_price_before_change(self):
+        if self.price_history.count() > 1:
+            queryset = self.price_history.all().order_by("-date_created")
+            return queryset[1].field_value
         else:
-            instance.__dict__[self.track_attribute_name].append(expanded_value)
-        
-        instance.__dict__[self._name] = value
+            raise ValueError("There is only one price for %s trip" % self.name)
 
-    def add_datetime_to_value(self, value):
-        return (value, datetime.now())
-
-    def values_between_dates(self, start: datetime, end: datetime) -> list:
-        values = self.__dict__[self.track_attribute_name]
-        return [x for x in values if start < x < end]
-    
 
 class TripModel(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
     name = models.CharField(max_length=128, null=False, blank=False)
     destination = models.CharField(max_length=128, null=False, blank=False)
     description = models.TextField(default="")
-    price = FloatFieldWithTracking(track_attribute_name="trip_price_history", null=False, blank=False)
+    price = models.OneToOneField(PriceModelWithTracker, on_delete=models.CASCADE)
     is_abroad = models.BooleanField(null=False, blank=False)
     contact_number = models.CharField(max_length=9, null=False, blank=False)
-    
-    def min_price_between_dates(self, start:datetime, end:datetime):
-        return min(self.price.values_between_dates(start, end))
-            
-    def get_last_price(self):
-        return self.price.track_attribute_name[-2]
-    
