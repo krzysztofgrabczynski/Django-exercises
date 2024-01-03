95bfd1775f1d989548333cda6d0008f22399d351
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-01-03 23:08:22 +0100

    Add SIGNUP_REDIRECT_URL to settings.py for redirecting if user is authenticated

--- TripIdeas_project/core/settings.py
+++ TripIdeas_project/core/settings.py
@@ -124,6 +124,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
-# Redirecting for login and logout
+# Redirecting 
 LOGIN_REDIRECT_URL = reverse_lazy("home")
 LOGOUT_REDIRECT_URL = reverse_lazy("login")
+SIGNUP_REDIRECT_URL = reverse_lazy("home") 
