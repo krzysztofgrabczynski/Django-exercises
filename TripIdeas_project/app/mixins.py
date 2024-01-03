da3e8610d02269b17d8a58188b022c61c1e87fa9
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-01-03 23:11:56 +0100

    New mixin for redirecting if user is authenticated (logged in)

--- TripIdeas_project/app/mixins.py
+++ TripIdeas_project/app/mixins.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect
 from functools import wraps
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponseRedirect
+from django.conf import settings
 
 
 def redirect_if_logged_user(func):
@@ -15,3 +18,33 @@ def redirect_if_logged_user(func):
         return func(request, *args, **kwargs)
 
     return wrapper
+
+
+class RedirectIfLoggedUserMixin:
+    redirect_field_name = None
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return self.redirect_authenticated_user()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_redirect_field_name(self):
+        # if hasattr(settings, 'SIGNUP_REDIRECT_URL'):
+        redirect_field_name = (
+            self.redirect_field_name or settings.SIGNUP_REDIRECT_URL
+            if hasattr(settings, "SIGNUP_REDIRECT_URL")
+            else self.redirect_field_name
+        )
+        # else:
+        #     redirect_field_name = self.redirect_field_name
+
+        if not redirect_field_name:
+            raise ImproperlyConfigured(
+                f"{self.__class__.__name__} is missing a redirect_field_name attribute. "
+                f"Please define {self.__class__.__name__}.redirect_field_name or override "
+                f"{self.__class__.__name__}.get_redirect_field_name()."
+            )
+        return str(redirect_field_name)
+
+    def redirect_authenticated_user(self):
+        return HttpResponseRedirect(self.get_redirect_field_name())
