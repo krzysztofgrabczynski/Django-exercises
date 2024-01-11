d544f4185be64b8bcfdc784fe51a5c37da9b3b94
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-01-11 01:22:53 +0100

    New mixin class: ObjectOwnerRequiredMixin. It checks if logged user is an owner of the specific object

--- TripIdeas_project/app/mixins.py
+++ TripIdeas_project/app/mixins.py
@@ -3,6 +3,7 @@ from functools import wraps
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponseRedirect
 from django.conf import settings
+from django.contrib.auth.mixins import AccessMixin
 
 
 def redirect_if_logged_user(func):
@@ -45,3 +46,29 @@ class RedirectIfLoggedUserMixin:
 
     def redirect_authenticated_user(self):
         return HttpResponseRedirect(self.get_redirect_field_name())
+
+
+class ObjectOwnerRequiredMixin(AccessMixin):
+    permission_denied_message = (
+        "You have to be the owner of the object you are trying to delete."
+    )
+    owner_field_name = None
+
+    def _get_object_owner(self):
+        assert self.owner_field_name is not None, (
+            f"`ObjectOwnerRequiredMixin.owner_field_name` cannot be None."
+            f"You must override `ObjectOwnerRequiredMixin.owner_field_name` in {self.__class__.__name__} class."
+        )
+
+        fields = [field.name for field in self.model._meta.fields]
+        if not self.owner_field_name in fields:
+            raise ImproperlyConfigured(
+                f"{self.model} needs to define `{self.owner_field_name}` field."
+            )
+
+        return getattr(self.get_object(), self.owner_field_name)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user == self._get_object_owner():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
