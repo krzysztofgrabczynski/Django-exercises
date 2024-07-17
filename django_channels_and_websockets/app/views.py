9f337935a1cb20f5e6ed1e86afd661ba3a996833
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-18 00:30:21 +0200

    Create test home view

--- django_channels_and_websockets/app/views.py
+++ django_channels_and_websockets/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+from .tasks import add
+
+
+def home(request):
+    add.delay(2, 2)
+    return HttpResponse("Done")
