730d0d4212fda0c3cb1793d9b02d7be571fde6d1
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-18 15:04:33 +0200

    Code refactor

--- django_channels_and_websockets/app/urls.py
+++ django_channels_and_websockets/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("chat/", views.chat, name="chat"),
 ]
--- django_channels_and_websockets/app/views.py
+++ django_channels_and_websockets/app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
-
-from .tasks import add
 
 
-def home(request):
-    add.delay(2, 2)
-    return HttpResponse("Done")
+def chat(request):
+    return render(request, "chat.html")
