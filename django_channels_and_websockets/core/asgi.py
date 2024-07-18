86a1dff57bd2180413c22dca2e5daeb539213d0c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-18 15:05:13 +0200

    Add configuration of the channels

--- django_channels_and_websockets/core/asgi.py
+++ django_channels_and_websockets/core/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter, ProtocolTypeRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+import app.routing
 
-application = get_asgi_application()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(app.routing.websocket_urlpatterns)
+        ),
+    }
+)
