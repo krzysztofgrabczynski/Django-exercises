ad5ed3c600a13165d6852686e0df532972ada723
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-20 17:10:23 +0200

    Add group in websocket consumer

--- django_channels_and_websockets/app/consumers.py
+++ django_channels_and_websockets/app/consumers.py
@@ -3,13 +3,27 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name,
+        )
+        await self.accept()
 
-    def disconnect(self, code):
-        pass
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        self.send(json.dumps({"message": message}))
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
