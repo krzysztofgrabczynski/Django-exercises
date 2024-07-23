971f2b46a764d2eb8cee9e31e728a8d29f42a90e
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-23 12:36:50 +0200

    Create new consumer for image download websocket

--- django_channels_and_websockets/app/consumers.py
+++ django_channels_and_websockets/app/consumers.py
@@ -27,3 +27,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps(event))
+
+
+class ImageDownloadConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = self.scope["url_route"]["kwargs"]["client_uuid"]
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def send_notification(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({
+            'message': message 
+        }))
