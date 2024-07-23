46949873504e9ddd3eb11b0f576254bc89556d89
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-23 12:35:46 +0200

    Create url for download image

--- django_channels_and_websockets/app/urls.py
+++ django_channels_and_websockets/app/urls.py
@@ -4,5 +4,6 @@ from app import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("download_image/", views.download_image, name="download-image"),
     path("<str:room_name>/", views.chat, name="room"),
 ]
