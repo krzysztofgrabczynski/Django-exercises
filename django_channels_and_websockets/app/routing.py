9449d01b03975db70be19c9d9102159815668bee
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-23 12:36:02 +0200

    Create url for websocket

--- django_channels_and_websockets/app/routing.py
+++ django_channels_and_websockets/app/routing.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from app.consumers import ChatConsumer
+from app.consumers import ChatConsumer, ImageDownloadConsumer
 
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
+    re_path(r"ws/notification/(?P<client_uuid>[0-9a-f-]{36})/$", ImageDownloadConsumer.as_asgi()),
 ]
